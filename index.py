9d7e87c97abe698e295118fc4840b6c38f869e60
Programador Novato <info@programadornovato.com>
2020-08-16 02:19:20 -0500

    Buscar un documento con Python en mongoDB (📊Curso de MongoDB y Python [10] ) 👈👈👈
    Video:
    https://youtu.be/4BuC78CikAU

--- index.py
+++ index.py
@@ -16,12 +16,19 @@ cliente=pymongo.MongoClient(MONGO_URI,serverSelectionTimeoutMS=MONGO_TIEMPO_FUER
 baseDatos=cliente[MONGO_BASEDATOS]
 coleccion=baseDatos[MONGO_COLECCION]
 ID_ALUMNO=""
-def mostrarDatos():
+def mostrarDatos(nombre="",sexo="",calificacion=""):
+    objetoBuscar={}
+    if len(nombre)!=0:
+        objetoBuscar["nombre"]=nombre
+    if len(sexo)!=0:
+        objetoBuscar["sexo"]=sexo
+    if len(calificacion)!=0:
+        objetoBuscar["calificacion"]=calificacion
     try:
         registros=tabla.get_children()
         for registro in registros:
             tabla.delete(registro)
-        for documento in coleccion.find():
+        for documento in coleccion.find(objetoBuscar):
             tabla.insert('',0,text=documento["_id"],values=documento["nombre"])
         cliente.close()
     except pymongo.errors.ServerSelectionTimeoutError as errorTiempo:
@@ -88,6 +95,8 @@ def borrarRegistro():
     editar["state"]="disabled"
     borrar["state"]="disabled"
     mostrarDatos()
+def buscarRegistro():
+    mostrarDatos(buscarNombre.get(),buscarSexo.get(),buscarCalificacion.get())
 ventana=Tk()
 tabla=ttk.Treeview(ventana,columns=2)
 tabla.grid(row=1,column=0,columnspan=2)
@@ -95,28 +104,43 @@ tabla.heading("#0",text="ID")
 tabla.heading("#1",text="NOMBRE")
 tabla.bind("<Double-Button-1>",dobleClickTabla)
 #Nombre
-Label(ventana,text="Nombre").grid(row=2,column=0)
+Label(ventana,text="Nombre").grid(row=2,column=0,sticky=W+E)
 nombre=Entry(ventana)
-nombre.grid(row=2,column=1)
+nombre.grid(row=2,column=1,sticky=W+E)
+nombre.focus()
 #Sexo
-Label(ventana,text="Sexo").grid(row=3,column=0)
+Label(ventana,text="Sexo").grid(row=3,column=0,sticky=W+E)
 sexo=Entry(ventana)
-sexo.grid(row=3,column=1)
+sexo.grid(row=3,column=1,sticky=W+E)
 #Calificacion
-Label(ventana,text="Calificacion").grid(row=4,column=0)
+Label(ventana,text="Calificacion").grid(row=4,column=0,sticky=W+E)
 calificacion=Entry(ventana)
-calificacion.grid(row=4,column=1)
+calificacion.grid(row=4,column=1,sticky=W+E)
 #Boton crear
 crear=Button(ventana,text="Crear alumno",command=crearRegistro,bg="green",fg="white")
-crear.grid(row=5,columnspan=2)
+crear.grid(row=5,columnspan=2,sticky=W+E)
 #Boton editar
 editar=Button(ventana,text="Editar alumno",command=editarRegistro,bg="yellow")
-editar.grid(row=6,columnspan=2)
+editar.grid(row=6,columnspan=2,sticky=W+E)
 editar["state"]="disabled"
 #Boton borrar
 borrar=Button(ventana,text="Borrar alumno",command=borrarRegistro,bg="red",fg="white")
-borrar.grid(row=7,columnspan=2)
+borrar.grid(row=7,columnspan=2,sticky=W+E)
 borrar["state"]="disabled"
+#buscar Nombre
+Label(ventana,text="Buscar por nombre").grid(row=8,column=0,sticky=W+E)
+buscarNombre=Entry(ventana)
+buscarNombre.grid(row=8,column=1,sticky=W+E)
+#Sexo
+Label(ventana,text="Buscar por Sexo").grid(row=9,column=0,sticky=W+E)
+buscarSexo=Entry(ventana)
+buscarSexo.grid(row=9,column=1,sticky=W+E)
+#Calificacion
+Label(ventana,text="Buscar por calificacion").grid(row=10,column=0,sticky=W+E)
+buscarCalificacion=Entry(ventana)
+buscarCalificacion.grid(row=10,column=1,sticky=W+E)
+buscar=Button(ventana,text="Buscar alumno",command=buscarRegistro,bg="blue",fg="white")
+buscar.grid(row=11,columnspan=2,sticky=W+E)
 
 mostrarDatos()
 ventana.mainloop()
